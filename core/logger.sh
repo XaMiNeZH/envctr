e4a2f670773c854cd168350c1ba9decaeff6664d
Marwa MACHACH <marwamachach@gmail.com>
2026-05-05 14:50:55 +0100

    feat: implement logger function

--- core/logger.sh
+++ core/logger.sh
@@ -1,1 +1,21 @@
-ÿþ
+#!/bin/bash
+LOG_DIR="${LOG_DIR:-/var/log/envctr}"
+LOG_FILE="$LOG_DIR/history.log"
+
+
+log_message(){
+    local TYPE="$1"
+    local MSG="$2"
+
+    mkdir -p "$LOG_DIR"
+
+    local TIMESTAMP
+    TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
+
+    local USERNAME
+    USERNAME=$(whoami)
+
+    local LINE="$TIMESTAMP : $USERNAME : $TYPE : $MSG"
+
+    echo "$LINE" | tee -a "$LOG_FILE"
+}
